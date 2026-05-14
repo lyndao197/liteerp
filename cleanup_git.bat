4fd88c925f348a316df623698974db22fe8cfc73
Duc Manh <ducmanh2303@gmail.com>
2026-05-15 00:34:54 +0700

    Remove temporary push script

---
+++ cleanup_git.bat
@@ -0,0 +1,4 @@
+del push_changes.bat
+git add .
+git commit -m "Remove temporary push script"
+git push
--- push_changes.bat
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "Complete Order Management module with fixes and optimizations"
-git push
